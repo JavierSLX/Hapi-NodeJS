81c2a206c0c9ec152a594f16efa39b39248cc71e
JavierSLX <jserranolule@gmail.com>
2019-06-03 21:34:31 -0500

    Plugin inert

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hapi
+
+* EL paquete inert es un plugin que le añade funcionalidad al objeto ``h`` y permite responder con un archivo o directorio entero.
--- platzi-overflow/index.js
+++ platzi-overflow/index.js
@@ -1,38 +1,50 @@
 'use strict';
 const hapi = require('hapi');
+const inert = require('inert');
+const path = require('path');
 
-//Intanciamos un servidor
+//Intanciamos un servidor (Define las rutas de los archivos desde el directorio donde se encuentra hasta public)
 const server = hapi.server({
     port: process.env.PORT || 3000,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+        files: {
+            relativeTo: path.join(__dirname, 'public')
+        }
+    }
 });
 
 //Función para definir la ruta del servidor y arrancarlo
 async function init()
 {
-    //El objeto h es una coleccion de utilidades y propiedades a la información de respuesta (importantes: h.response, h.redirect) de response se tienen response.header, response.type, response.code
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            //Regresa la respuesta con un codigo 200
-            return h.response('Hola mundo...').code(200);
-        }
-    });
-
-    //Nueva ruta que redirecciona
-    server.route({
-        method: 'GET',
-        path: '/redirect',
-        handler: (request, h) => {
-            //Regresa la respuesta con un codigo 200
-            return h.redirect('https://platzi.com');
-        }
-    });
-
     //Arranca el servidor
     try
     {
+        //Definimos que vamos a usar el plugin de inert
+        await server.register(inert);
+
+        //El objeto h es una coleccion de utilidades y propiedades a la información de respuesta (importantes: h.response, h.redirect) de response se tienen response.header, response.type, response.code
+        server.route({
+            method: 'GET',
+            path: '/home',
+            handler: (request, h) => {
+                //Regresa la respuesta con un codigo 200
+                return h.file('index.html');
+            }
+        });
+
+        //Nueva ruta que redirecciona
+        server.route({
+            method: 'GET',
+            path: '/{param*}',
+            handler: {
+                directory: {
+                    path: '.',
+                    index: ['index.html']
+                }
+            }
+        });
+
         await server.start();
     }catch(error)
     {
